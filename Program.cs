a21845cdbd1a7ec849c11307b4d9067203156a2c
Daniel Soots <danielsoots@icloud.com>
2022-01-20 12:46:54 -0500

    completed collection practice

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace CollectionsPractice
 {
@@ -6,7 +7,51 @@ namespace CollectionsPractice
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int [] zeroNine = {0,1,2,3,4,5,6,7,8,9};
+            bool [] yesNo ={true,false,true,false,true,false,true,false,true,false};
+
+
+            string [] names = {"Tim", "Martin", "Nikki", "Amanda"};
+            List<string> iceCreamFlavors = new List<string>();
+            iceCreamFlavors.Add("Strawberry");
+            iceCreamFlavors.Add("Chocolate");
+            iceCreamFlavors.Add("Vanilla");
+            iceCreamFlavors.Add("Mint Chocolate");
+            iceCreamFlavors.Add("Orange");
+
+            System.Console.WriteLine($"We have {iceCreamFlavors.Count} flavors of icecream.");
+            System.Console.WriteLine($"The third flavor is: {iceCreamFlavors[2]}");
+            iceCreamFlavors.Remove(iceCreamFlavors[2]);
+            System.Console.WriteLine($"We have {iceCreamFlavors.Count} flavors of icecream.");
+
+
+            Dictionary<string,string> kv = new Dictionary<string, string>();
+
+            Random rand = new Random();
+
+            foreach (string name in names) {
+                kv.Add(name, iceCreamFlavors[rand.Next(0, iceCreamFlavors.Count)]);
+            }
+
+            foreach (var rum in kv)
+            {
+                System.Console.WriteLine(rum.Key + " ** " + rum.Value);
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         }
     }
 }
