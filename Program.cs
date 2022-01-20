c3f4503da1c98e76ecbea9f64e1243bf2c6b1d94
Daniel Soots <danielsoots@icloud.com>
2022-01-20 13:24:08 -0500

    practice with multidiminsonal arrays

--- Program.cs
+++ Program.cs
@@ -40,9 +40,48 @@ namespace CollectionsPractice
 
 
 
+            // Multidimensional arrays
+            int [,] array2D = new int[3,2];
+            int [,,] array3D = new int[2,3,4]
+            {
+                {   {45, 1, 16, 17}, {4, 47, 21, 68}, {21, 28, 32, 76} },
+                {   {11, 0, 85, 42}, {9, 10, 14, 96}, {66, 99, 33, 12} }
+            };
 
+            System.Console.WriteLine(array2D[0,1]); // prints 0
+            System.Console.WriteLine(array3D[1,0,3]); // prints 42
 
+            int [][] jaggedArray = new int[3][];
+            jaggedArray[0] = new int[5];
+            jaggedArray[1] = new int[4];
+            jaggedArray[2] = new int[2];
+            
+            int[][] jaggedArray2 = new int[][] {
+                new int[] {1,3,5,7,9},
+                new int[] {0,2},
+                new int[] {11,22,33,44}
+            };
 
+            int[][] jaggedArray3 = {
+                new int[] {1,3,5,7,9},
+                new int[] {4,6},
+                new int[] {55,66,77,88}
+            };
+
+            int[][,] jaggedArray4 = new int[3][,]
+            {
+                new int[,] { {1,3}, {5,7} },
+                new int[,] { {0,8}, {4,6}, {8,10} },
+                new int[,] { {99,11}, {55,33}, {98, 16}}
+            };
+
+            foreach (int[] innerArr in jaggedArray)
+            {
+                System.Console.WriteLine("Inner array length is {0}", innerArr.Length);
+            }
+
+            System.Console.WriteLine(jaggedArray2[0][1]); // 3
+            System.Console.WriteLine(jaggedArray3[2][3]); // 88
 
 
 
